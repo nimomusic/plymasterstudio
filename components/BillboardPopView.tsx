92e74d0719085897903604de1e1400d322090258
nimomusic <nimomusicceo@gmail.com>
2026-09-03 19:24:06 +0900

    Update BillboardPopView.tsx

--- components/BillboardPopView.tsx
+++ components/BillboardPopView.tsx
@@ -2295,6 +2295,7 @@ export const BillboardPopView: React.FC<BillboardPopViewProps> = ({ setView, sta
             <span className="text-white/80 font-bold">(주)니모뮤직</span>
             <span className="text-white/20">|</span>
             <span>이메일: <a href="mailto:plymaster.help@gmail.com" className="text-white/70 hover:text-purple-300 underline underline-offset-2 transition-colors">plymaster.help@gmail.com</a></span>
+            <span className="text-white/20">|</span>
             <span>홈페이지: <a href="www.plymaster.co.kr" className="text-white/70 hover:text-purple-300 underline underline-offset-2 transition-colors">www.plymaster.co.kr</a></span>
           </div>
           <p className="text-[11px] text-white/30 tracking-wide mt-1">
